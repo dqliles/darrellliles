4dfa14b23c69ee055140e75c7f4f2641971c527d
dqliles <dqliles@yahoo.com>
2018-05-22 19:34:29 -0400

    initial portfolio rebuild

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+showMenu() {
+  document.getElementById('nav').style.display = 'block';
+  document.getElementById('btn').style.display = 'none';
+  document.getElementById('x').style.display = 'block';
+};
+
+hideMenu() {
+  document.getElementById('nav').style.display = 'none';
+  document.getElementById('btn').style.display = 'block';
+  document.getElementById('x').style.display = 'none';
+
+}
